f2ff13378d871146b3cd8689d52de6851cf71308
Alya Aziz-Zaman <alya.aziz.zaman@gmail.com>
2016-03-21 09:56:59 -0600

    added api routes for users

--- Nomadica-back-end/routes/users.js
+++ Nomadica-back-end/routes/users.js
@@ -7,27 +7,65 @@ var knex = require('../db/knex');
 
 //POST new user
 router.post('/', function(req, res) {
-  console.log(req.body);
   var user = req.body;
   knex('users').insert({
     user_name: user.user_name,
     email: user.email,
     password: user.password
   })
-  .then(function(success){
+  .then(function(data){
     res.sendStatus(200);
+    console.log(data);
   })
   .catch(function(error) {
     res.sendStatus(500);
+    console.log(error);
   });
 });
 
 //GET user by id
 router.get('/:id', function(req, res) {
-  var user = req.body;
-  knex('users').
-
+  knex('users')
+    .where('id', req.params.id)
+    .then(function(data) {
+      res.sendStatus(200);
+      console.log(data);
     })
-})
+    .catch(function(error) {
+      res.sendStatus(500);
+      console.log(error);
+    });
+});
+
+//DELETE user
+router.delete('/:id', function(req, res) {
+  knex('users')
+    .where('id', req.params.id)
+    .del()
+    .then(function() {
+      res.sendStatus(200);
+      console.log("User deleted");
+    });
+});
+
+//UPDATE user account info
+router.put('/:id', function(req, res) {
+  var user = req.body;
+  knex('users')
+    .where('id', req.params.id)
+    .update({
+      user_name: user.user_name,
+      email: user.email,
+      password: user.password
+    .then(function(data){
+      res.sendStatus(200);
+      console.log(data);
+    })
+    .catch(function(error) {
+      res.sendStatus(500);
+      console.log(error);
+    })
+  });
+});
 
 module.exports = router;
